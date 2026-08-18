f06d1638408328e0d9ec6dabace83b0efbe31645
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-18 17:28:45 +0530

    style: format auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -18,10 +18,8 @@ import { JwtStrategy } from './jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    JwtStrategy,
-  ],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
+
