559958b2a8c546f4299056fb35b6b246ef6a599c
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-26 20:52:09 +0530

    feat(subscription): add admin subscriptions page

---
+++ admin/src/app/subscriptions/page.tsx
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import AdminShell from "@/components/admin/admin-shell";
+import { ACCESS_COOKIE, REFRESH_COOKIE } from "@/lib/auth";
+import SubscriptionsClient from "./subscriptions-client";
+
+export default async function SubscriptionsPage() {
+  const cookieStore = await cookies();
+  const hasSession =
+    cookieStore.has(ACCESS_COOKIE) || cookieStore.has(REFRESH_COOKIE);
+
+  if (!hasSession) redirect("/login");
+
+  return (
+    <AdminShell>
+      <SubscriptionsClient />
+    </AdminShell>
+  );
+}
