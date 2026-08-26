f78d0cad62b7a6918830f33c1286195af4591c58
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-26 19:06:54 +0530

    feat(wallet): register accounting policy configuration

--- backend/src/platform-config/platform-config.module.ts
+++ backend/src/platform-config/platform-config.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SecurityConfigModule } from '../security-config/security-config.module';
+import { AccountingConfigController } from './accounting-config.controller';
+import { AccountingConfigService } from './accounting-config.service';
 import { PlatformConfigController } from './platform-config.controller';
 import { PlatformConfigService } from './platform-config.service';
 
 @Module({
   imports: [SecurityConfigModule],
-  controllers: [PlatformConfigController],
-  providers: [PlatformConfigService],
-  exports: [PlatformConfigService],
+  controllers: [PlatformConfigController, AccountingConfigController],
+  providers: [PlatformConfigService, AccountingConfigService],
+  exports: [PlatformConfigService, AccountingConfigService],
 })
 export class PlatformConfigModule {}
