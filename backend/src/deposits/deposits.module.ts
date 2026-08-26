26cadab62cb24557f65a7b3d64c84acbc75803e7
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-26 15:08:58 +0530

    feat(deposits): register payment-rail controller

--- backend/src/deposits/deposits.module.ts
+++ backend/src/deposits/deposits.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AdminDepositAccountsController } from './admin-deposit-accounts.controller';
+import { AdminDepositPaymentRailsController } from './admin-deposit-payment-rails.controller';
 import { AdminDepositsController } from './admin-deposits.controller';
 import { DepositsController } from './deposits.controller';
 import { DepositsService } from './deposits.service';
@@ -7,6 +8,7 @@ import { DepositsService } from './deposits.service';
 @Module({
   controllers: [
     DepositsController,
+    AdminDepositPaymentRailsController,
     AdminDepositAccountsController,
     AdminDepositsController,
   ],
