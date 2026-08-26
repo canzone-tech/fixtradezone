0f97af14845c56319436ccd5df70ff0114b20676
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-26 17:33:56 +0530

    feat(wallet): register wallet ledger module

---
+++ backend/src/wallet/wallet.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import {
+  AdminDepositAccountingController,
+  AdminLedgerController,
+} from './admin-ledger.controller';
+import { AdminWalletsController } from './admin-wallets.controller';
+import { WalletLedgerService } from './wallet-ledger.service';
+import { WalletController } from './wallet.controller';
+
+@Module({
+  controllers: [
+    WalletController,
+    AdminWalletsController,
+    AdminLedgerController,
+    AdminDepositAccountingController,
+  ],
+  providers: [WalletLedgerService],
+  exports: [WalletLedgerService],
+})
+export class WalletModule {}
