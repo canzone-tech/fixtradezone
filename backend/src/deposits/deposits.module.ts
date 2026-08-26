aede990e38957cebddeb93acd95c4fa22965c391
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-26 17:34:53 +0530

    feat(wallet): wire deposit approvals to wallet ledger service

--- backend/src/deposits/deposits.module.ts
+++ backend/src/deposits/deposits.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { WalletModule } from '../wallet/wallet.module';
 import { AdminDepositAccountsController } from './admin-deposit-accounts.controller';
 import { AdminDepositPaymentRailsController } from './admin-deposit-payment-rails.controller';
 import { AdminDepositsController } from './admin-deposits.controller';
@@ -6,6 +7,7 @@ import { DepositsController } from './deposits.controller';
 import { DepositsService } from './deposits.service';
 
 @Module({
+  imports: [WalletModule],
   controllers: [
     DepositsController,
     AdminDepositPaymentRailsController,
