8f205c62e98817eb1d310c8687a77b416be67a06
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-08-28 09:03:32 +0530

    feat(ops): wire rewards into manual activation

--- backend/src/subscriptions/subscriptions.module.ts
+++ backend/src/subscriptions/subscriptions.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CommissionsModule } from '../commissions/commissions.module';
+import { RewardsModule } from '../rewards/rewards.module';
 import {
   AdminDepositSubscriptionController,
   AdminSubscriptionsController,
@@ -8,7 +9,7 @@ import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 
 @Module({
-  imports: [CommissionsModule],
+  imports: [CommissionsModule, RewardsModule],
   controllers: [
     SubscriptionsController,
     AdminSubscriptionsController,
