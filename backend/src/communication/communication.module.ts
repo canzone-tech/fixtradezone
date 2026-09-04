e4874f3ebed343b995ccf219d577411419ff7487
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-04 17:59:37 +0530

    feat(communication): register superadmin email operations

--- backend/src/communication/communication.module.ts
+++ backend/src/communication/communication.module.ts
@@ -1,9 +1,13 @@
 import { Global, Module } from '@nestjs/common';
+import { SecurityConfigModule } from '../security-config/security-config.module';
+import { CommunicationAdminController } from './communication-admin.controller';
 import { CommunicationService } from './communication.service';
 import { EmailTransportService } from './email-transport.service';
 
 @Global()
 @Module({
+  imports: [SecurityConfigModule],
+  controllers: [CommunicationAdminController],
   providers: [CommunicationService, EmailTransportService],
   exports: [CommunicationService],
 })
