b355f266b748b99120a904084d3317385a52a3f7
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-05 14:10:10 +0530

    feat(cms): wire managed email template service

--- backend/src/communication/communication.module.ts
+++ backend/src/communication/communication.module.ts
@@ -1,14 +1,20 @@
 import { Global, Module } from '@nestjs/common';
+import { ContentModule } from '../content/content.module';
 import { SecurityConfigModule } from '../security-config/security-config.module';
 import { CommunicationAdminController } from './communication-admin.controller';
 import { CommunicationService } from './communication.service';
 import { EmailTransportService } from './email-transport.service';
+import { ManagedEmailTemplateService } from './managed-email-template.service';
 
 @Global()
 @Module({
-  imports: [SecurityConfigModule],
+  imports: [SecurityConfigModule, ContentModule],
   controllers: [CommunicationAdminController],
-  providers: [CommunicationService, EmailTransportService],
-  exports: [CommunicationService],
+  providers: [
+    CommunicationService,
+    EmailTransportService,
+    ManagedEmailTemplateService,
+  ],
+  exports: [CommunicationService, ManagedEmailTemplateService],
 })
 export class CommunicationModule {}
