19cca963401e9ad3bfb23857034650a01dc54bc5
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-10 15:38:47 +0530

    feat(deposits): wire approval mode automation

--- backend/src/deposits/deposits.module.ts
+++ backend/src/deposits/deposits.module.ts
@@ -5,13 +5,17 @@ import { RewardsModule } from '../rewards/rewards.module';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { AdminDepositAccountsController } from './admin-deposit-accounts.controller';
+import { AdminDepositApprovalModeController } from './admin-deposit-approval-mode.controller';
 import { AdminDepositBlockchainConfigController } from './admin-deposit-blockchain-config.controller';
 import { AdminDepositBlockchainVerificationController } from './admin-deposit-blockchain-verification.controller';
 import { AdminDepositPackageAccountsController } from './admin-deposit-package-accounts.controller';
 import { AdminDepositPaymentRailsController } from './admin-deposit-payment-rails.controller';
 import { AdminDepositsController } from './admin-deposits.controller';
+import { DepositApprovalModeService } from './deposit-approval-mode.service';
 import { DepositApprovalOrchestratorService } from './deposit-approval-orchestrator.service';
 import { DepositBlockchainApprovalGuardService } from './deposit-blockchain-approval-guard.service';
+import { DepositBlockchainAutoApprovalWorker } from './deposit-blockchain-auto-approval.worker';
+import { DepositBlockchainProcessingService } from './deposit-blockchain-processing.service';
 import { DepositBlockchainVerificationService } from './deposit-blockchain-verification.service';
 import { DepositPackageRoutingService } from './deposit-package-routing.service';
 import { DepositSubmissionOrchestratorService } from './deposit-submission-orchestrator.service';
@@ -32,6 +36,7 @@ import { PackageDepositFlowService } from './package-deposit-flow.service';
     DepositsController,
     AdminDepositPaymentRailsController,
     AdminDepositBlockchainConfigController,
+    AdminDepositApprovalModeController,
     AdminDepositAccountsController,
     AdminDepositPackageAccountsController,
     AdminDepositsController,
@@ -42,11 +47,18 @@ import { PackageDepositFlowService } from './package-deposit-flow.service';
     PackageDepositFlowService,
     DepositPackageRoutingService,
     DepositBlockchainVerificationService,
+    DepositApprovalModeService,
     DepositBlockchainApprovalGuardService,
+    DepositBlockchainProcessingService,
     DepositSubmissionOrchestratorService,
     DirectDepositApprovalService,
     DepositApprovalOrchestratorService,
+    DepositBlockchainAutoApprovalWorker,
   ],
-  exports: [DepositsService, DepositBlockchainVerificationService],
+  exports: [
+    DepositsService,
+    DepositBlockchainVerificationService,
+    DepositApprovalModeService,
+  ],
 })
 export class DepositsModule {}
