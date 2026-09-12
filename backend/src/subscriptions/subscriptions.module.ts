8391e9006050b1bebb96f1a1ffae339b4461b27e
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-12 21:12:55 +0530

    feat(payouts): export total wallet reinvestment engine

--- backend/src/subscriptions/subscriptions.module.ts
+++ backend/src/subscriptions/subscriptions.module.ts
@@ -10,6 +10,7 @@ import { SubscriptionActivationOrchestratorService } from './subscription-activa
 import { SubscriptionPostActivationService } from './subscription-post-activation.service';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
+import { TotalWalletPackagePurchaseService } from './total-wallet-package-purchase.service';
 
 @Module({
   imports: [CommissionsModule, RewardsModule, InternalTradingModule],
@@ -22,7 +23,12 @@ import { SubscriptionsService } from './subscriptions.service';
     SubscriptionsService,
     SubscriptionPostActivationService,
     SubscriptionActivationOrchestratorService,
+    TotalWalletPackagePurchaseService,
   ],
-  exports: [SubscriptionsService, SubscriptionPostActivationService],
+  exports: [
+    SubscriptionsService,
+    SubscriptionPostActivationService,
+    TotalWalletPackagePurchaseService,
+  ],
 })
 export class SubscriptionsModule {}
